463b2e04d1efc777c504f11135d346d849cd1453
Jose Portillo <alu0101568232@ull.edu.es>
2023-10-31 01:56:10 +0000

    Advance in script

--- scdebug.sh
+++ scdebug.sh
@@ -11,11 +11,20 @@ TRACE_NAME="trace_"$(uuidgen)".txt"
 #### Functions
 help_text() {}
 error_text() {}
-
+just_program() {
+  program="$1"
+  shift
+  strace -o .scdebug/"$program"/$TRACE_NAME "$program $@"
+}
 
 #### Main Program
 cat << _EOF_
+if [ -n "$1" ]; then
+    if command -v "$1" &>/dev/null; then
+        just_program()
+    else if ["$1" == "--help" ] || ["$1" == "-h"]; then
 
-
-
+    else if ["$1" == "sto"]
+      
+fi
 _EOF_
