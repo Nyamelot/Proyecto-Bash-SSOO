6ae71d9a3131f6fa5ac3f2ac983d2de0815b7cb8
Jose Portillo <alu0101568232@ull.edu.es>
2023-11-05 17:23:00 +0000

    Added while to read parameters

--- scdebug.sh
+++ scdebug.sh
@@ -4,27 +4,47 @@
 
 #### Constants
 
-
-TRACE_NAME="trace_"$(uuidgen)".txt"
+UUID=$(uuidgen)
+TRACE_NAME="trace_$UUID.txt"
+TRACE_PATH="$HOME/.scdebug/$program"
+CURRENT_TRACE_PATH="$HOME/.scdebug/$program/$TRACE_NAME"
 
 
 #### Functions
-help_text() {}
-error_text() {}
-just_program() {
-  program="$1"
-  shift
-  strace -o $HOME/.scdebug/"$program"/$TRACE_NAME "$program $@"
+help_text() {
+  echo ""
+}
+error_text() {
+  echo ""
+}
+usage() {
+  echo ""
+}
+options() {
+  while [[ $1 != "" ]]; do
+    case $1 in
+      -sto )
+        shift
+        echo "$1"
+        ;;
+      -h | --help)
+        shift
+        echo "$1"
+        # comprobar si hay un valor, si no lo hay -> error
+        # comprabar si el valor continue un "-", si lo continue es que
+        # tampoco hay un valor
+        pepe+="$1 "
+        ;;
+      * )
+        # argumento desconocido - error
+        shift
+        ;;
+    esac
+  done
 }
 
 #### Main Program
-cat << _EOF_
-if [ -n "$1" ]; then
-    if command -v "$1" &>/dev/null; then
-        just_program()
-    else if ["$1" == "--help" ] || ["$1" == "-h"]; then
 
-    else if ["$1" == "sto"]
-      
-fi
-_EOF_
+
+options $@
+
