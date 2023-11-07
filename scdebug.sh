4edd1278a34a7f6061afd7cc6d81c87674aa5ed6
Jose Portillo <alu0101568232@ull.edu.es>
2023-11-07 07:11:35 +0000

    Finish b & c 4.1

--- scdebug.sh
+++ scdebug.sh
@@ -30,7 +30,6 @@ options() {
       -sto )
         shift
         sto_options=$1
-        echo $sto_options
         shift
         ;;
       -h | --help)
@@ -39,60 +38,106 @@ options() {
         break
         ;;
         -nattch)
-          echo $1
-          name_pid
+          nattch=true
           shift
+          name_pid $@
         ;;
         -pattch)
-          pid_list
+          pattch=true
+          shift
+          pid_list $@
+        ;;
+        -v)
+          if [ -n "$program" ]; then
+            trace_file=$(basename $(find $HOME/.scdebug/$program/*.txt -type f -exec stat -c '%Y %n' {} + | sort -n | head -n 1 | awk '{print $2}'))
+            file_time=$(stat $(find $HOME/.scdebug/$program/*.txt -type f -exec stat -c '%Y %n' {} + | sort -n | head -n 1 | awk '{print $2}') | grep Modify | sed 's/Modify: //')
+            echo "===== Command: $program ====="
+            echo "===== Trace File: $trace_file ====="
+            echo "===== Time: $file_time ====="
+          else 
+            program=$2
+            trace_file=$(basename $(find $HOME/.scdebug/$program/*.txt -type f -exec stat -c '%Y %n' {} + | sort -n | head -n 1 | awk '{print $2}'))
+            file_time=$(stat $(find $HOME/.scdebug/$program/*.txt -type f -exec stat -c '%Y %n' {} + | sort -n | head -n 1 | awk '{print $2}') | grep Modify | sed 's/Modify: //')
+            echo "===== Command: $program ====="
+            echo "===== Trace File: $trace_file ====="
+            echo "===== Time: $file_time ====="
+          fi
+          exit 0
+        ;;
+        -vall)
+          if [ -n "$program" ]; then
+            trace_files=($(find $HOME/.scdebug/$program -type f))
+            for trace_file in "${trace_files[@]}"; do
+              file_name=$(basename $trace_file)
+              file_time=$(stat $trace_file | grep Modify | sed 's/Modify: //')
+              echo "===== Command $program ====="
+              echo "===== Trace File: $file_name ====="
+              echo "===== Time: $file_time ====="
+              echo ""
+            done
+          else
+            program=$2
+            trace_files=($(find $HOME/.scdebug/$program -type f))
+            for trace_file in "${trace_files[@]}"; do
+              file_name=$(basename $trace_file)
+              file_time=$(stat $trace_file | grep Modify | sed 's/Modify: //')
+              echo "===== Command $program ====="
+              echo "===== Trace File: $file_name ====="
+              echo "===== Time: $file_time ====="
+              echo ""
+            done
+          fi
+          exit 0
         ;;
       * )
         if command -v "$1" > /dev/null 2>&1; then
           program="$1"
           shift
           program_parameters=()
-          while [ "$1" != "-sto" ] && [ "$1" != "-h" ] && [ "$1" != "--help" ] && [ "$1" != "-natcch" ] && [ "$1" != "-pattch" ] && [ "$1" != "" ]; do
+          while [ "$1" != "-sto" ] && [ "$1" != "-h" ] && [ "$1" != "--help" ] && [ "$1" != "-natcch" ]  && [ "$1" != "-pattch" ] && [ "$1" != "" ] && [ "$1" != "-v" ] && [ "$1" != "-vall" ]; do
             program_parameters+=($1)
             shift
           done
+        elif [ $2 == "-v" ]; then
+          program=$1
+          shift
         else
           shift
         fi
         ;;
     esac
   done
-  program_attach
+  if [ -n "$nattch" ] || [ -n "$pattch" ]; then
+    program_attach
+  else
+    program_tracing
+  fi
 }
 program_tracing(){
   if [ -n "$sto_options" ]; then
     mkdir -p "$HOME/.scdebug/$program/"
-    echo "${program_parameters[@]}"
-    echo "$sto_options"
-    strace "$sto_options" -o "$HOME/.scdebug/$program/$TRACE_NAME" "$program" "${program_parameters[@]}"
+    strace "$sto_options" -o "$HOME/.scdebug/$program/$TRACE_NAME" "$program" "${program_parameters[@]}" &
   else
     mkdir -p "$HOME/.scdebug/$program/"
-    strace -o "$HOME/.scdebug/$program/$TRACE_NAME" "$program" "${program_parameters[@]}"
+    strace -o "$HOME/.scdebug/$program/$TRACE_NAME" "$program" "${program_parameters[@]}" &
   fi
 }
 program_attach(){
   if [ -n "$sto_options" ]; then
     for program in "${!programs_pid[@]}"; do
       mkdir -p "$HOME/.scdebug/${programs_pid[$program]}/"
-      strace "$sto_options" -p "$program" -o "$HOME/.scdebug/${programs_pid[$program]}/$TRACE_NAME"
+      strace "$sto_options" -p "$program" -o "$HOME/.scdebug/${programs_pid[$program]}/$TRACE_NAME" &
     done
   else
     for program in "${!programs_pid[@]}"; do
       mkdir -p "$HOME/.scdebug/${programs_pid[$program]}/"
-      strace -p "$program" -o "$HOME/.scdebug/${programs_pid[$program]}/$TRACE_NAME"
+      strace -p "$program" -o "$HOME/.scdebug/${programs_pid[$program]}/$TRACE_NAME" &
     done
   fi
 }
 name_pid() {
-  echo "hola"
-  echo "$1"
   while [ "$1" != "-sto" ] && [ "$1" != "-h" ] && [ "$1" != "--help" ] &&  [ "$1" != "-pattch" ] && [ "$1" != "" ]; do
     programs_pid["$(pgrep $1 | tr ' ' '\n' | tail -n 1)"]="$1"
-    echo $1
     shift
   done
 }
@@ -106,6 +151,7 @@ pid_list(){
 
 #### Main Program
   options $@
+  
 
 
 
